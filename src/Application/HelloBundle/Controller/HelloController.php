5b04ae2be03b5ae8aa15c934f8f7b3e3e25ac2f0
Vincent Jousse <vincent.jousse@devorigin.fr>
2010-08-24 09:52:59 +0200

    Fixed controller namespace

--- src/Application/HelloBundle/Controller/HelloController.php
+++ src/Application/HelloBundle/Controller/HelloController.php
@@ -2,7 +2,7 @@
 
 namespace Application\HelloBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller;
 
 class HelloController extends Controller
 {
